5772366e3755b1e5a6023b8ee8ea9d1416370eb3
lamas1987 <105235207+lamas1987@users.noreply.github.com>
2025-11-04 00:30:36 +0100

    Update service worker to version 3 with new assets

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-const CACHE = 'calc-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE = 'calc-v3';
+const ASSETS = ['./','./index.html','./manifest.webmanifest',
+  './apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
+));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
